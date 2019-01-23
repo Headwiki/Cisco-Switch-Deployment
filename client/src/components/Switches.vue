53a750ba49c9d0585a201545eabbc60105c08ca0
Henrik Hedlund Andersen <henrik@laa015-dgi011.ad.dgi.no>
2019-01-23 15:47:53 +0100

    connection status for dhcp server

--- client/src/components/Switches.vue
+++ client/src/components/Switches.vue
@@ -1,8 +1,17 @@
 <template>
+<div>
+      <div v-if="status == 'connecting'">
+      <p>Kobler til {{ this.url }}</p>
+      <div class="loader"></div>
+    </div>
+    <p v-else-if="status == 'connected'">Koblet til {{ this.url }}</p>
+    <p v-else-if="status == 'not connected'">Kunne ikke koble til {{ this.url }}</p>
+    <p v-else>Error</p>
   <table class>
     <thead>
       <tr>
         <th>IP-adresse</th>
+        <th>Image</th>
         <th>Status</th>
         <th>Valg</th>
       </tr>
@@ -11,16 +20,12 @@
       <tr v-for="ip in ips" :key="ip">
         <td>{{ ip }}</td>
         <td></td>
-        <!-- <td v-if="ip.status">
-          <span class="label label-success">Online</span>
-        </td>
-        <td v-else>
-          <span class="label label-danger">Offline</span>
-        </td>-->
+        <td></td>
         <td>valg</td>
       </tr>
     </tbody>
   </table>
+  </div>
 </template>
 <script>
 const axios = require("axios");
@@ -29,7 +34,9 @@ export default {
   name: "Switches",
   data() {
     return {
-      ips: []
+      ips: [],
+      url: "http://localhost:8080", // http://10.8.1.4/cgi-bin/dhcp.cgi
+      status: 'connecting'
     };
   },
   created() {
@@ -37,9 +44,20 @@ export default {
   },
   methods: {
     loadData () {
-      axios.get("http://10.8.1.0/cgi-bin/dhcp.cgi")
+      axios({
+        method: 'get',
+        url: this.url,
+        timeout: 5000
+      })
       .then((res) => {
-        this.ips = ["1.1.1.1"]
+        this.status = 'connected';
+        if(res.data != '')
+          this.ips = ["2.2.2.2"]
+        else
+          this.ips = ["1.1.1.1"]
+      })
+      .catch((error) => {
+        this.status = 'not connected'
       })
     },
     getStatus() {}
@@ -47,4 +65,18 @@ export default {
 };
 </script>
 <style>
+.loader {
+  display: inline-block;
+  border: 2px solid #f3f3f3; /* Light grey */
+  border-top: 2px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
 </style>
