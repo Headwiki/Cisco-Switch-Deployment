1d2f1b458f5e5b161bd85c57e5bff91d375002ec
Henrik Hedlund Andersen <henrik@laa015-dgi011.ad.dgi.no>
2019-01-25 10:36:04 +0100

    more vue

---
+++ client/src/components/SwitchDevice.vue
@@ -0,0 +1,29 @@
+<template>
+  <tr>
+    <td>{{ ipd }}</td>
+    <td>{{ image }}</td>
+    <td>{{ status }}</td>
+    <td>{{ action }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "SwitchDevice",
+  props: {
+    ip: String
+  },
+  data() {
+    return {
+      ipd: this.ip,
+      image: 'image',
+      status: 'status',
+      action: 'action'
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
--- client/src/components/Switches.vue
+++ client/src/components/Switches.vue
@@ -1,70 +1,71 @@
 <template>
-<div>
-      <div v-if="status == 'connecting'">
+  <div>
+    <div v-if="status == 'connecting'">
       <p>Kobler til {{ this.url }}</p>
       <div class="loader"></div>
     </div>
     <p v-else-if="status == 'connected'">Koblet til {{ this.url }}</p>
     <p v-else-if="status == 'not connected'">Kunne ikke koble til {{ this.url }}</p>
     <p v-else>Error</p>
-  <table class>
-    <thead>
-      <tr>
-        <th>IP-adresse</th>
-        <th>Image</th>
-        <th>Status</th>
-        <th>Valg</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="ip in ips" :key="ip">
-        <td>{{ ip }}</td>
-        <td></td>
-        <td></td>
-        <td>valg</td>
-      </tr>
-    </tbody>
-  </table>
+    <table>
+      <thead>
+        <tr>
+          <th>IP-adresse</th>
+          <th>Image</th>
+          <th>Status</th>
+          <th>Handlinger</th>
+        </tr>
+      </thead>
+      <tbody>
+        <SwitchDevice v-for="ip in ips" :ip="ip" :key="ip"/>
+      </tbody>
+    </table>
   </div>
 </template>
 <script>
+import SwitchDevice from "./SwitchDevice.vue";
 const axios = require("axios");
 
 export default {
   name: "Switches",
+  components: {
+    SwitchDevice
+  },
   data() {
     return {
       ips: [],
-      url: "http://localhost:8080", // http://10.8.1.4/cgi-bin/dhcp.cgi
-      status: 'connecting'
+      url: "http://10.8.0.4/cgi-bin/dhcp.cgi", // http://10.8.1.4/cgi-bin/dhcp.cgi
+      status: "connecting"
     };
   },
   created() {
     this.loadData();
   },
   methods: {
-    loadData () {
+    loadData() {
       axios({
-        method: 'get',
+        method: "get",
         url: this.url,
         timeout: 5000
       })
-      .then((res) => {
-        this.status = 'connected';
-        if(res.data != '')
-          this.ips = ["2.2.2.2"]
-        else
-          this.ips = ["1.1.1.1"]
-      })
-      .catch((error) => {
-        this.status = 'not connected'
-      })
+        .then(res => {
+          this.status = "connected";
+          if (res.data != "") this.ips = ["2.2.2.2", "3.3.3.3"];
+          else this.ips = ["1.1.1.1"];
+        })
+        .catch(error => {
+          this.status = "not connected";
+        });
     },
     getStatus() {}
   }
 };
 </script>
 <style>
+table {
+  margin: auto;
+}
+
 .loader {
   display: inline-block;
   border: 2px solid #f3f3f3; /* Light grey */
@@ -76,7 +77,15 @@ export default {
 }
 
 @keyframes spin {
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
+
+.hidden {
+    visibility: hidden;
+  }
 </style>
